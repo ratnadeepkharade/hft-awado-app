e878023d171343ebd0ffd7ab31de90f253393da2
Ratnadeep Rajendra Kharade <92khra1mst@hft-stuttgart.de>
2019-11-26 14:02:48 +0100

    Integrated login with api call

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -9,20 +12,33 @@ import { Router } from '@angular/router';
 export class LoginPage implements OnInit {
   username = "";
   password = "";
-  correctCredentials=false;
+  correctCredentials = false;
+  loginApi: Observable<any>;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public httpClient: HttpClient) {
 
+  }
 
   ngOnInit() {
   }
-  clickMe() {
-    if (this.username === "admin" && this.password === "admin" || this.username === "demo" && this.password === "demo") {
-
-      this.router.navigateByUrl('/home');
-    }
-    else { this.correctCredentials=true; }
 
 
+  login() {
+    this.loginApiCall();
+  }
+
+  loginApiCall() {
+    this.loginApi = this.httpClient.post('http://193.196.52.237:8081/authenticate', {
+      "email": this.username,
+      "password": this.password
+    });
+    this.loginApi
+      .subscribe((data) => {
+        console.log('my data: ', data);
+        this.router.navigateByUrl('/home');
+      }, (error) =>  {
+        console.log(error);
+        this.correctCredentials = true;
+      });
   }
 }
